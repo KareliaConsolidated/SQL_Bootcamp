c5c464b4723ce24688e3fcfbded6e192efcf4c0b
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-02 11:01:53 +0530

    Creating Databases

---
+++ 01_Creating_Databases.sql
@@ -0,0 +1,30 @@
+SHOW DATABASES; # Lists the Current Databases in MySQL Server
+
+/*+--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+6 rows in set (0.00 sec)*/
+
+# To Create Database: CREATE DATABASE <name>;
+CREATE DATABASE testing_db;
+
+/*mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| testing_db         | <- 
+| world              |
++--------------------+
+7 rows in set (0.00 sec)*/
