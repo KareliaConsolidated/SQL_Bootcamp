b25c72c6d914d4562fc9adb2ce2e8a30250da18a
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-02 11:02:25 +0530

    Dropping Databases

---
+++ 02_Dropping_Databases.sql
@@ -0,0 +1,17 @@
+# DROP DATABASE <name>; 
+
+mysql> DROP DATABASE testing_db;
+# Query OK, 0 rows affected (0.01 sec)
+
+/*mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+6 rows in set (0.00 sec)*/
